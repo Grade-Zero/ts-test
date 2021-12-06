b5fc722c7cbefb70b8872b56172cb415fb85e5e0
Ben Cream <b.cream87@gmail.com>
2021-12-06 23:46:01 +1000

    Implement getArnieQuotes function

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,16 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+const REAL_QUOTE = "Arnie Quote";
+const NO_QUOTE = "FAILURE";
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
-};
+type TResult = { [REAL_QUOTE]: string } | { [NO_QUOTE]: string };
+
+const getQuote = (url: string): Promise<TResult> =>
+  httpGet(url).then((quote) =>
+    quote.status === 200
+      ? { [REAL_QUOTE]: JSON.parse(quote.body).message }
+      : { [NO_QUOTE]: JSON.parse(quote.body).message }
+  );
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> =>
+  Promise.all(urls.map((url: string) => getQuote(url)));
